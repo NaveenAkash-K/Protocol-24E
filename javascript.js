80332b67edb14c7aca9af75b8ea5e0c9d31d4226
NaveenAkash-K <64596821+NaveenAkash-K@users.noreply.github.com>
2023-06-11 18:51:27 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function openmenu(){
+    document.getElementById("menu").style.visibility="visible";
+    document.getElementById("menu").style.opacity=1;
+    // document.getElementById("menu").style.display="block";
+
+}
+
+function closemenu(){
+    document.getElementById("menu").style.visibility="hidden";
+    document.getElementById("menu").style.opacity=0;
+    // document.getElementById("menu").style.display="none";
+
+}
